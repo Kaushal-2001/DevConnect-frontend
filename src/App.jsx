6b1b5fee083ed56da2b78925dfc3f2f8c0d29672
Kaushal Apastambh <apastambhkaushal2001@gmail.com>
2026-07-05 17:26:21 -0700

    Update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import Login from "./pages/login";
+import { Login } from "./pages/Login";
 import Profile from "./pages/Profile";
+import { Signup } from "./pages/Signup";
 
 function App() {
   return (
     <>
       <BrowserRouter basename="/">
         <Routes>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/login" element={<Login/>} />
           <Route path="/" element={<Layout />} >
-            <Route path="/login" element={<Login />} />
             <Route path="/Profile" element={ <Profile/>} />
           </Route>
         </Routes>
