7b3eb59972a0f08ca91e9c4cac8b7260955569f8
Kaushal Apastambh <apastambhkaushal2001@gmail.com>
2026-07-08 23:55:36 -0700

    Add route for feed page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { Login } from "./pages/Login";
 import Profile from "./pages/Profile";
 import { Signup } from "./pages/Signup";
 import { Provider } from "react-redux";
-import {appStore}  from "./utils/appStore";
+import { appStore } from "./utils/appStore";
+import Feed from "./pages/Feed";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Layout />}>
+              <Route path="/" element={<Feed/>}/>
               <Route path="/Profile" element={<Profile />} />
             </Route>
           </Routes>
